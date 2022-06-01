5ecf7fea7c698efe0b1411ae9b11286eeb9ee814
Nelson-Ot <onnellsson@gmail.com>
2022-06-01 12:07:18 +0300

    about section completed
    about section completed

--- frontend/src/container/About/About.jsx
+++ frontend/src/container/About/About.jsx
@@ -1,13 +1,42 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+
+import { motion } from 'framer-motion';
+
+import { images } from '../../constants';
 
 import './About.scss';
 
+
+const abouts = [
+
+    {title: 'Web Development', description: 'I am a web developer', imgUrl: images.about01 },
+    {title: 'Frontend Development', description: 'html, css javascript', imgUrl: images.about02 },
+    {title: 'UX/UI', description: 'figma, adobe xd', imgUrl: images.about03 },
+    {title: 'Backend development', description: 'Javascript, php', imgUrl: images.about04 },
+];
+
 const About = () => {
     return (
-        <div>
-        <h1>About</h1>
-        <p>This is the TodoList app v1.0.0. It is part of a React crash course</p>
+        <>
+        <h2 className="head-text">I know that <span>Good Dev</span> <br /> means <span> Good Business</span> </h2>
+        <div className="app__profiles">
+            {/* <h2>Test Write</h2> */}
+            {abouts.map((about, index) => (
+                <motion.div
+                whileInView={{opaicty: 1}}
+                whileHover={{scale: 1.1}}
+                transition={{duration: 1, type: 'tween'}}
+                className="app__profile-item"
+                key={about.title + index}
+
+                >
+                    <img src={about.imgUrl} alt="profile" />
+                    <h2 className="bold-text" style={{marginTop: 20}} >{about.title}</h2>
+                    <p className='p-text' style={{marginTop: 10}}>{about.description}</p>
+                </motion.div>
+            ))}
         </div>
+        </>
     );
     };
 
