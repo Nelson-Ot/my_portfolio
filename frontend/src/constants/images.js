c79e691d1f7040a1f7642d1714b60643bafd39b7
Nelson-Ot <onnellsson@gmail.com>
2022-06-12 00:19:00 +0300

    complete portfolio
    complete portfolio

--- frontend/src/constants/images.js
+++ frontend/src/constants/images.js
@@ -24,6 +24,7 @@ import about03 from '../assets/about03.png';
 import about04 from '../assets/about04.png';
 
 import profile from '../assets/Nelson.png';
+import nelson from '../assets/cleans.png';
 // import profile from '../assets/profile.png';
 import circle from '../assets/circle.svg';
 import logo from '../assets/logos.png';
@@ -62,6 +63,7 @@ export default {
   about03,
   about04,
   profile,
+  nelson,
   circle,
   logo,
   adidas,
