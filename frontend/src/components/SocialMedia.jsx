b57304d99c2fef82a29d106e0955026b3313999c
Nelson-Ot <onnellsson@gmail.com>
2022-06-16 12:39:36 +0300

    added social media links
    added social media links

--- frontend/src/components/SocialMedia.jsx
+++ frontend/src/components/SocialMedia.jsx
@@ -7,15 +7,22 @@ import { FaFacebookF } from 'react-icons/fa';
 const SocialMedia = () => {
   return (
     <div className="app__social">
+        <a href="https://twitter.com/sito_arez" target="_blank" rel="noreferrer">
+
         <div>
             <BsTwitter />
         </div>
+        </a>
+        <a href="https://www.instagram.com/sito_arez/" target="_blank" rel="noreferrer">
         <div>
             <BsInstagram />
         </div>
+        </a>
+        <a href="https://www.facebook.com/nelson.otieno.54/" target="_blank" rel="noreferrer">
         <div>
             <FaFacebookF />
         </div>
+        </a>
       
 
     </div>
